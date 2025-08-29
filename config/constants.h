8c1221f597c89c2f8e2e5cf620739f1ab64fd587
Ironbunz <mrj1072@gmail.com>
2025-08-28 21:08:46 -0400

    8/828 updating file structure

---
+++ config/constants.h
@@ -0,0 +1,34 @@
+#pragma once
+
+// Keys
+#define ___ &trans
+#define XXX &none
+
+// Layers
+// Must match order in which they are defined
+// DEFINE LAYERS
+#define Base        0
+#define Nav         1
+#define Fun         2
+#define Game        3
+#define Gameplus    4
+#define Mouse       5
+#define Sys         6
+
+// Modifier keys
+#define GL LGUI
+#define CL LCTRL
+#define AL LALT
+#define SL LSHFT
+#define GR RGUI
+#define CR RCTRL
+#define AR RALT
+#define SR RSHFT
+
+// OS
+#define WINDOWS 0
+#define LINUX 1
+#define MAC_OS 2
+
+//Apps
+//#define TMUX LC(LA(J)) // Tmux -> Ctrl + Alt + J
