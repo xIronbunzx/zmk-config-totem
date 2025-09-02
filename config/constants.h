f0496f604dbb3c6ed541f9335f86188093c2d0bd
Ironbunz <mrj1072@gmail.com>
2025-09-02 11:07:12 -0400

    9/02 updating file structure

---
+++ config/constants.h
@@ -0,0 +1,19 @@
+#pragma once
+
+// DEFINE LAYERS
+	#define Base        0
+	#define Nav         1
+	#define Fun         2
+	#define Game        3
+	#define Gameplus    4
+	#define Mouse       5
+	#define Sys         6
+
+    // DEFINE Trans/None behavior
+	#define ___ &trans
+	#define XXX &none
+
+/* Home Row Mods */
+		#define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4 LB5      // left-hand keys
+		#define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4 RB5      // right-hand keys
+		#define THUMBS LH2 LH1 LH0 RH0 RH1 RH2                                              // thumb keys
